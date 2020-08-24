9490267f8deae5a007344254abd27bdc227f2e2a
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-08-24 18:54:12 +0200

    Implement WkipediaDailyEventScreapper

--- lib/scrappers/wikipedia/daily_events.py
+++ lib/scrappers/wikipedia/daily_events.py
@@ -1,9 +1,13 @@
 import datetime
 import locale
+import re
 import time
 from collections import defaultdict
 
+from bs4 import BeautifulSoup
+
 from lib.scrappers.base.base_scrapper import BaseScrapper
+from lib.utils import fetch
 
 date_handlers = defaultdict(lambda: lambda x: x.strftime('%B_%d'))
 date_handlers['de'] = lambda x: x.strftime('%d._%B')
@@ -22,7 +26,37 @@ class WikipediaDailyEventScrapper(BaseScrapper):
         super(WikipediaDailyEventScrapper, self).__init__()
 
     def scrap(self, *args, **kwargs):
-        pass
+        soup = fetch.fetch_html(self._target_uri)
+        bullets = soup.select('#bodyContent li')
+
+        whitelist = kwargs.get('whitelist', [])
+        blacklist = kwargs.get('blacklist', [])
+
+        results = {}
+
+        for bullet in bullets:
+            text = bullet.text.strip().lower()
+
+            if not re.match("^\d+", text):
+                continue
+
+            if any(word.lower() in text for word in blacklist):
+                continue
+
+            if any(word.lower() in text for word in whitelist):
+                bullet_section = bullet.parent.find_previous_sibling('h2')
+
+                if not bullet_section:
+                    continue
+
+                bullet_title = bullet_section.contents[0].get_text()
+
+                if bullet_title not in results:
+                    results[bullet_title] = []
+
+                results[bullet_title].append(bullet)
+
+        return results
 
     def _get_target_uri(self):
         locale.setlocale(locale.LC_TIME, self._locale)
